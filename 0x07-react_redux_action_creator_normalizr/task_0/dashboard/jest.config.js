4b2617b480a92e63a1b8bf0c3b4dbacd1f4b7798
evalase219 <chiomaevangeline85@gmail.com>
2024-05-10 03:53:36 +0200

    Read data from a JSON

--- 0x07-react_redux_action_creator_normalizr/task_0/dashboard/jest.config.js
+++ 0x07-react_redux_action_creator_normalizr/task_0/dashboard/jest.config.js
@@ -4,9 +4,9 @@ module.exports = {
   testEnvironment: 'jsdom',
   moduleNameMapper: {
     // ... Existing rules...
-    '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$': '/home/evalase/alx-react/0x04-React_inline_styling/task_4/dashboard/__mocks__/fileMock.js',  // New line
-    '\\.(css|less)$': '/home/evalase/alx-react/0x04-React_inline_styling/task_4/dashboard/__mocks__/styleMock.js',
+    '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$': '/home/evalase/alx-react/0x07-react_redux_action_creator_normalizr/task_0/dashboard/__mocks__/fileMock.js',  // New line
+    '\\.(css|less)$': '/home/evalase/alx-react/0x07-react_redux_action_creator_normalizr/task_0/dashboard/__mocks__/styleMock.js',
   },
-  setupFilesAfterEnv: ['/home/evalase/alx-react/0x04-React_inline_styling/task_4/dashboard/config/setupTests.js'],
-  '^enzyme$': '/home/evalase/alx-react/0x04-React_inline_styling/task_4/dashboard/node_modules/enzyme', // Map 'enzyme' to its actual path
+  setupFilesAfterEnv: ['/home/evalase/alx-react/0x07-react_redux_action_creator_normalizr/task_0/dashboard/config/setupTests.js'],
+  '^enzyme$': '/home/evalase/alx-react/0x07-react_redux_action_creator_normalizr/task_0/dashboard/node_modules/enzyme', // Map 'enzyme' to its actual path
   };
